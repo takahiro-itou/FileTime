8f6074e7fe07c5109107499e9c935cb6158d695e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-11-29 20:09:24 +0900

    ヘッダファイルを追加

---
+++ Core/Include/FileTime/Common/FileTimeSettings.h
@@ -0,0 +1,31 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  File Time Tools.  ---                      **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      プロジェクトの設定。
+**
+**      @file       Common/FileTimeSettings.h
+**/
+
+#if !defined( FILETIME_COMMON_INCLUDED_FILE_TIME_SETTINGS_H )
+#    define   FILETIME_COMMON_INCLUDED_FILE_TIME_SETTINGS_H
+
+//  スクリプトによる設定値が書き込まれたヘッダを読み込む。  //
+#include    "FileTime/.Config/ConfiguredFileTime.h"
+
+FILETIME_NAMESPACE_BEGIN
+
+FILETIME_NAMESPACE_END
+
+#endif
