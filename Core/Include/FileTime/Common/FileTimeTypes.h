236f2022f59bdecf565fe2499a9317f07dc5df7a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-12-18 17:47:24 +0900

    ヘッダファイルを追加

---
+++ Core/Include/FileTime/Common/FileTimeTypes.h
@@ -0,0 +1,70 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  File Time Tools.  ---                      **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/FileTimeTypes.h
+**/
+
+#if !defined( FILETIME_COMMON_INCLUDED_FILE_TIME_TYPES_H )
+#    define   FILETIME_COMMON_INCLUDED_FILE_TIME_SETTINGS_H
+
+#include    "FileTimeSettings.h"
+
+
+FILETIME_NAMESPACE_BEGIN
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+//----------------------------------------------------------------
+/**
+**    エラーコード。
+**/
+
+enum  ErrCode
+{
+    /**   正常終了。    **/
+    ERR_SUCCESS             = 0,
+
+    /**   異常終了。エラーの理由は不明または報告なし。  **/
+    ERR_FAILURE             = 1,
+
+    /**   ファイルオープンエラー。  **/
+    ERR_FILE_OPEN_ERROR     = 2,
+
+    /**   ファイル入出力エラー。    **/
+    ERR_FILE_IO_ERROR       = 3,
+
+    /**   パーミッションエラー。    **/
+    ERR_PERMISSION_ERROR    = 4
+};
+
+//----------------------------------------------------------------
+/**
+**    型安全なブール型。
+**/
+
+enum  Boolean
+{
+    BOOL_FALSE  = 0,        /**<  偽。  **/
+    BOOL_TRUE   = 1         /**<  真。  **/
+};
+
+FILETIME_NAMESPACE_END
+
+#endif
