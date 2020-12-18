0346f10b4abc2d781ad5f67d3cf55fabb74119be
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-12-18 19:22:51 +0900

    クラスを新規作成

---
+++ Core/Include/FileTime/Common/ErrorDetectionCode.h
@@ -0,0 +1,125 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  File Time Tools.  ---                      **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ErrorDetectionCode class.
+**
+**      @file       Common/ErrorDetectionCode.h
+**/
+
+#if !defined( FILETIME_COMMON_INCLUDED_ERROR_DETECTION_CODE_H )
+#    define   FILETIME_COMMON_INCLUDED_ERROR_DETECTION_CODE_H
+
+#include    "FileTimeTypes.h"
+
+#include    <iosfwd>
+#include    <string>
+
+FILETIME_NAMESPACE_BEGIN
+namespace  Common  {
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    ErrorDetectionCode  class.
+//
+
+class  ErrorDetectionCode
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+public:
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    ErrorDetectionCode();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~ErrorDetectionCode();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   ErrorDetectionCodeTest;
+};
+
+}   //  End of namespace  Common
+FILETIME_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/Common/ErrorDetectionCode.cpp
@@ -0,0 +1,90 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  File Time Tools.  ---                      **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of ErrorDetectionCode class.
+**
+**      @file       Common/ErrorDetectionCode.cpp
+**/
+
+#include    "FileTime/Common/ErrorDetectionCode.h"
+
+FILETIME_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ErrorDetectionCode  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+ErrorDetectionCode::ErrorDetectionCode()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+ErrorDetectionCode::~ErrorDetectionCode()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+FILETIME_NAMESPACE_END
